54a4c330b3bbe93e9404f320f5bc6cf290aba0e5
Gorcenski <Gorcenski@users.noreply.github.com>
2018-06-15 07:28:04 -0400

    Update extraction script and the street csv to be more complete - this uses a different filter that is more accurate and not based on actual addresses, but rather the names of different ways

--- data/extract.sh
+++ data/extract.sh
@@ -16,5 +16,6 @@ echo "Generating osm file. This will take several minutes..."
 osmosis --read-pbf-fast berlin-latest.osm.pbf file="berlin-latest.osm.pbf" --bounding-polygon file="berlin.poly" --write-xml file="berlin.osm"
 
 echo "Generating csv file..."
-osmfilter berlin.osm --keep="addr:country= and addr:city= and addr:street=" --ignore-dependencies --drop-relations --drop-ways |osmconvert - --csv="@oname @id @lon @lat addr:country addr:city addr:street" > berlin-streets.csv
+osmfilter berlin.osm --keep="highway=*" --ignore-dependencies --drop-relations --drop-nodes | osmconvert - --csv="@oname @id highway name" > berlin-streets.csv
+
 
